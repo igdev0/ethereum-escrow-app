b2c9993c8f2c9022246a3fd3ee19203e400c720b
igdev <dorultanianos@gmail.com>
2025-04-11 10:37:03 +0300

    chore: remove old styles

--- app/contracts-list/layout.tsx
+++ app/contracts-list/layout.tsx
@@ -2,8 +2,8 @@ import {PropsWithChildren} from 'react';
 
 export default function Layout(props: PropsWithChildren) {
   return (
-      <div className="w-full min-h-full bg-amber-300">
+      <>
         {props.children}
-      </div>
+      </>
   )
 }
