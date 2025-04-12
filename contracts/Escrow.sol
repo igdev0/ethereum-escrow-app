b9d428aaaf9e189b3a57faa787d1f6e271105505
igdev <dorultanianos@gmail.com>
2025-04-12 11:49:16 +0300

    fix: requirements issue

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -18,11 +18,12 @@ contract Escrow {
     modifier BasicRequirements {
         _;
         require(msg.sender != beneficiary, "You cannot be the arbiter and beneficiary at the same time!");
-        require(msg.sender != depositor, "You cannot be the arbiter and depositor at the same time!");
+        require(arbiter != depositor, "You cannot be the arbiter and depositor at the same time!");
     }
 
     function approve() external {
         require(msg.sender == arbiter, "You must be the arbiter in order to approve!");
+        require(msg.sender != depositor, "You cannot be the arbiter and depositor at the same time!");
         require(!approved, "This Escrow is been approved already!");
         uint balance = address(this).balance;
         (bool sent, ) = address(beneficiary).call{value: balance }("");
