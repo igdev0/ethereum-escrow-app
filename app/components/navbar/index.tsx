012008f8708587c9f22b2e7d955248675764e84c
igdev <dorultanianos@gmail.com>
2025-04-12 10:44:09 +0300

    feat: add view contracts route

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -1,10 +1,14 @@
+"use client";
 import Logout from '@/app/components/logout';
 import Link from 'next/link';
+import {usePathname} from 'next/navigation';
 
 export default function Navbar() {
+  const pathname = usePathname();
   return (
       <div className="flex justify-end gap-2 py-2">
-        <Link href="/create-escrow" className="navlink">Create new escrow</Link>
+        <Link href="/view-contracts" draggable={false} className={`navlink ${pathname === '/view-contracts' ? 'border-b-4 border-b-amber-500' : ""}`}>View contracts</Link>
+        <Link href="/create-escrow" draggable={false} className={`navlink ${pathname === '/create-escrow' ? 'border-b-4 border-b-amber-500' : ""}`}>Create new escrow</Link>
         <Logout/>
       </div>
   )
