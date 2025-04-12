198c58805ec813650389ed7d2e2febf05712d237
igdev <dorultanianos@gmail.com>
2025-04-12 10:33:46 +0300

    feat: add not found markup

--- app/components/list-contracts/index.tsx
+++ app/components/list-contracts/index.tsx
@@ -1,6 +1,7 @@
 import {PropsWithChildren} from 'react';
 import Address from '@/app/components/address';
 import Approve from '@/app/components/approve';
+import Link from 'next/link';
 
 interface ContractDataType {
   id: string;
@@ -23,39 +24,45 @@ export default async function ListContracts({contracts}: ListContractsProps) {
   return (
       <div>
         <h1 className="text-4xl font-bold my-5">Contracts</h1>
-        <table className="bg-white shadow-2xl rounded-sm">
-          <thead>
-          <tr>
-            <td className="font-bold p-2">Contract address</td>
-            <td className="font-bold p-2">Beneficiary</td>
-            <td className="font-bold p-2">Depositor</td>
-            <td className="font-bold p-2">Arbiter</td>
-          </tr>
-          </thead>
-          <tbody>
-          {
-            contracts.map(item => (
-                <tr key={item.id}>
-                  <td className="p-2">
-                    <Address address={item.address as string}/>
-                  </td>
-                  <td className="p-2">
-                    <Address address={item.beneficiary as string}/>
-                  </td>
-                  <td className="p-2">
-                    <Address address={item.depositor as string}/>
-                  </td>
-                  <td className="p-2">
-                    <Address address={item.arbiter as string}/>
-                  </td>
-                  <td className="p-2">
-                    <Approve contractAddress={item.address as string} arbiter={item.arbiter as string} isApproved={item.isApproved}/>
-                  </td>
-                </tr>
-            ))
-          }
-          </tbody>
-        </table>
+        {
+          contracts.length ? (
+
+                <table className="bg-white shadow-2xl rounded-sm">
+                  <thead>
+                  <tr>
+                    <td className="font-bold p-2">Contract address</td>
+                    <td className="font-bold p-2">Beneficiary</td>
+                    <td className="font-bold p-2">Depositor</td>
+                    <td className="font-bold p-2">Arbiter</td>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {
+                    contracts.map(item => (
+                        <tr key={item.id}>
+                          <td className="p-2">
+                            <Address address={item.address as string}/>
+                          </td>
+                          <td className="p-2">
+                            <Address address={item.beneficiary as string}/>
+                          </td>
+                          <td className="p-2">
+                            <Address address={item.depositor as string}/>
+                          </td>
+                          <td className="p-2">
+                            <Address address={item.arbiter as string}/>
+                          </td>
+                          <td className="p-2">
+                            <Approve contractAddress={item.address as string} arbiter={item.arbiter as string}
+                                     isApproved={item.isApproved}/>
+                          </td>
+                        </tr>
+                    ))
+                  }
+                  </tbody>
+                </table>
+          ) : <h1>No contracts found 😮 <Link className="text-amber-500 font-bold" href="/create-escrow">Create one</Link></h1>
+        }
       </div>
   );
 }
